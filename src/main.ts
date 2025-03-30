9f7f0e8f197c942523845c61428b409331104d26
André Saraiva Selva <121300349+andreselva@users.noreply.github.com>
2025-03-30 15:23:44 -0300

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,17 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log('🚀 HTTP API rodando na porta 3000');
+  console.log('HTTP API rodando na porta 3000');
 
   const microservice = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
     options: {
       host: 'localhost',
-      port: 3001, // Diferente da API principal
+      port: 3001,
     },
   });
   await microservice.listen();
-  console.log('🔌 Microserviço rodando na porta 3001');
+  console.log('Microserviço rodando na porta 3001');
 }
 
 bootstrap();
